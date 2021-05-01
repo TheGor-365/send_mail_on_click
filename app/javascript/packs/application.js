c4ea196f3c4087dfe044526dbf0affc9d5b08692
Gor <7495500@gmail.com>
2021-05-01 22:29:02 +0300

    Add basic app configure

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,21 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+
+import 'bootstrap'
+import '../stylesheets/application'
+import "@fortawesome/fontawesome-free/css/all.css"
+
+document.addEventListener("turbolinks:load", () => {
+  $('[data-toggle="tooltip"]').tooltip();
+  $('[data-toggle="popover"]').popover()
+})
+
+window.jQuery = $;
+window.$ = $;
+
+global.toastr = require("toastr")
 
 Rails.start()
 Turbolinks.start()
